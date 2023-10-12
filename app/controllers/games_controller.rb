b15348644f4c77874e1cb0f5fd40bff2d4c76255
Ritsuki Toshima <ricky0912t@gmail.com>
2023-10-12 18:58:56 +0900

    ok

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,9 @@
-require 'json'
-require 'open-uri'
-
 class GamesController < ApplicationController
+  require 'json'
+  require 'open-uri'
+
   def new
+    @start = Time.now()
     @letters = []
     10.times do
       @letters << ('A'..'Z').to_a.sample
@@ -11,16 +12,34 @@ class GamesController < ApplicationController
 
   def score
     @letters = params[:letters]
+    @time = params[:time]
+    @score = 0
     if !included?(params[:attempt].upcase, @letters)
-      @message = "Sorry but #{params[:attempt]} can't be built out of #{@letters}"
+      @message = "Sorry but '#{params[:attempt]}' can't be built out of #{@letters}"
     else
       if english_word?(params[:attempt])
-        @message = "Congraturations! #{params[:attempt]} is a valid English word!"
+        @message = "Congraturations! '#{params[:attempt]}' is a valid English word!"
+        @score = (params[:attempt].size * 10) / (@time.to_f * 1000)
       else
-        @message = "Sorry but #{params[:attempt]} does not seem to be a valid Englsih word..."
+        @message = "Sorry but '#{params[:attempt]}' does not seem to be a valid Englsih word..."
       end
     end
   end
+
+  # def score
+  #   @time = params[:time]
+  #   @score = 0
+  #   if self.included
+  #     if self.found
+  #       @result = "Congratulations! '#{params[:word].upcase}' is a valid English word!"
+  #       @score = (@word.size * 10) / (@time.to_f * 1000)
+  #     else
+  #       @result = "Sorry, but '#{params[:word].upcase}' does not appear to be a valid English word!"
+  #     end
+  #   else
+  #     @result = "Sorry but '#{params[:word].upcase}' can't be built out of #{params[:letters].split.join(', ')}"
+  #   end
+  # end
 
   def home
   end
